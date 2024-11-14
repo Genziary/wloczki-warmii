60ef520f8a7961cfb81d9a26ca7a2acbc773f6af
Martyna Ewa Koźbiał <121809496+mkozbial@users.noreply.github.com>
2024-11-14 19:31:31 +0100

    Carriers set up 📦⬆️ (#20)
    * 🚚 Set up carriers
    
    * 🐛 Fix typo

--- shop/modules/ps_buybuttonlite/vendor/autoload.php
+++ shop/modules/ps_buybuttonlite/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit0f1917fbd97a1d11d8acfe348458be2d::getLoader();
+return ComposerAutoloaderInit805f8b763ab6bdb27deb7974e3eeb755::getLoader();
