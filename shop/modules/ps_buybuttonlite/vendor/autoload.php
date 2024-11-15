56734c57907dc9dc39a75b5c644552560a1a3537
sztvk <sztvkowska@gmail.com>
2024-11-15 21:58:07 +0100

    Configure bank account details 💳

--- shop/modules/ps_buybuttonlite/vendor/autoload.php
+++ shop/modules/ps_buybuttonlite/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit805f8b763ab6bdb27deb7974e3eeb755::getLoader();
+return ComposerAutoloaderInit498898ffcb071f4aed51f42da8764548::getLoader();
