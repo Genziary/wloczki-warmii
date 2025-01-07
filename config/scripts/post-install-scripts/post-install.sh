cbec8ac44d7378efe9520e9b0d9a3b451dbc9201
gzukowski <85632612+gzukowski@users.noreply.github.com>
2025-01-07 22:58:07 +0100

    Added cache💾💾 (#62)
    * Added cache
    
    * corrected dump
    
    * working cache

--- config/scripts/post-install-scripts/post-install.sh
+++ config/scripts/post-install-scripts/post-install.sh
@@ -3,3 +3,10 @@
 echo "@ Initializing database with dump file..."
 
 mysql -uwloczki-user -pwloczki-password -h db wloczki-warmii < /tmp/sql/dump.sql
+
+echo "@ Enabling cache..."
+
+sed -i "s/'ps_caching' => 'CacheMemcache'/'ps_caching' => 'CacheMemcached'/" /var/www/html/app/config/parameters.php
+sed -i "s/'ps_cache_enable' => false/'ps_cache_enable' => true/" /var/www/html/app/config/parameters.php
+
+
