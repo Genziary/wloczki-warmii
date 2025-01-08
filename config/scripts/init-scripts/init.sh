693ad64eee3d68e29eacd696ee877dff0dcec803
Kamil Wenta <kamil.wenta222@gmail.com>
2025-01-08 21:12:22 +0100

    Fix updating SSL domain 🗝️

--- config/scripts/init-scripts/init.sh
+++ config/scripts/init-scripts/init.sh
@@ -3,6 +3,10 @@
 if [ "$USE_DB_DUMP" -eq "1" ] ; then
   echo "@ Loading database dump..."
   mysql -u"$DB_USER" -p"$DB_PASSWD" -h"$DB_SERVER" "$DB_NAME" < /tmp/sql/dump.sql
+
+  echo "@ Updating shop url..."
+  mysql -u"$DB_USER" -p"$DB_PASSWD" -h"$DB_SERVER" -e "UPDATE ps_configuration SET value = \"$PS_DOMAIN\" WHERE name LIKE \"%SHOP_DOMAIN%\"" "$DB_NAME" 
+  mysql -u"$DB_USER" -p"$DB_PASSWD" -h"$DB_SERVER" -e "UPDATE ps_shop_url SET domain = \"$PS_DOMAIN\", domain_ssl = \"$PS_DOMAIN\" WHERE id_shop = 1" "$DB_NAME" 
 else
   echo "@ Skipping database loading"
 fi
--- config/scripts/post-install-scripts/post-install.sh
+++ config/scripts/post-install-scripts/post-install.sh
@@ -4,6 +4,11 @@ echo "@ Initializing database with dump file..."
 
 mysql -u"$DB_USER" -p"$DB_PASSWD" -h"$DB_SERVER" "$DB_NAME" < /tmp/sql/dump.sql
 
+echo "@ Updating shop url..."
+
+mysql -u"$DB_USER" -p"$DB_PASSWD" -h"$DB_SERVER" -e "UPDATE ps_configuration SET value = \"$PS_DOMAIN\" WHERE name LIKE \"%SHOP_DOMAIN%\"" "$DB_NAME" 
+mysql -u"$DB_USER" -p"$DB_PASSWD" -h"$DB_SERVER" -e "UPDATE ps_shop_url SET domain = \"$PS_DOMAIN\", domain_ssl = \"$PS_DOMAIN\" WHERE id_shop = 1" "$DB_NAME" 
+
 echo "@ Enabling cache..."
 
 sed -i "s/'ps_caching' => 'CacheMemcache'/'ps_caching' => 'CacheMemcached'/" /var/www/html/app/config/parameters.php
