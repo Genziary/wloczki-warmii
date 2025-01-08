ca80e1f9e35da0133522c6cd05816d7cec33e19a
Kamil Wenta <36380628+kwerenta@users.noreply.github.com>
2025-01-08 11:15:11 +0100

    Fix Docker image to run as standalone 🐳 (#64)
    * Add modules and themes to Docker image 🐳
    
    * Fix scripts not using environment variables 🏞️

--- config/scripts/post-install-scripts/post-install.sh
+++ config/scripts/post-install-scripts/post-install.sh
@@ -2,11 +2,9 @@
 
 echo "@ Initializing database with dump file..."
 
-mysql -uwloczki-user -pwloczki-password -h db wloczki-warmii < /tmp/sql/dump.sql
+mysql -u"$DB_USER" -p"$DB_PASSWD" -h"$DB_SERVER" "$DB_NAME" < /tmp/sql/dump.sql
 
 echo "@ Enabling cache..."
 
 sed -i "s/'ps_caching' => 'CacheMemcache'/'ps_caching' => 'CacheMemcached'/" /var/www/html/app/config/parameters.php
 sed -i "s/'ps_cache_enable' => false/'ps_cache_enable' => true/" /var/www/html/app/config/parameters.php
-
-
